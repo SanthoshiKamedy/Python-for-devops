a4af8bde193efa39ba39ce53b163018d619a32c0
santhoshi kamedy <168489325+SanthoshiKamedy@users.noreply.github.com>
2025-03-06 23:49:31 +0000

    Create cal.py

---
+++ cal.py
@@ -0,0 +1,18 @@
+import os
+def addition(n1,n2):
+  add =n1+n2
+  return add
+def sub(n1,n2):
+  sub=n1-n2
+  return sub
+n1=float(sys.argv[1])
+operation=sys.argv[2]
+n2=float(sys.argv[3])
+if operation=="add"
+  output=add(num1,num2)
+print(output)
+if operation=="sub"
+  output(num1,num2)
+  print(output)
+
+
