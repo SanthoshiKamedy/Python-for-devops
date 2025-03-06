ee6425cbf505ed3e24799843b66949a50b5d3cdb
santhoshi kamedy <168489325+SanthoshiKamedy@users.noreply.github.com>
2025-03-06 23:54:33 +0000

    Create function.py

---
+++ function.py
@@ -0,0 +1,17 @@
+def addition(num1,um2)
+  add = num1+num2
+  return add
+def sub(num1,num2)
+  sub=num1-num2
+  return sub
+def mul(num1,num2)
+  mul=num1*num2
+  return mul
+def div(num1,num2)
+  div=num1/num2
+  return div
+print((addition(10,5))
+print((sub(10,5))
+print((mul(10,5))
+print((div(10,5))
+      
