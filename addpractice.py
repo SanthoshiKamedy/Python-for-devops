16d12ab659ccbd93a8a0d42cbfcc5b8dda8af853
santhoshi kamedy <168489325+SanthoshiKamedy@users.noreply.github.com>
2025-03-06 23:45:31 +0000

    Create addpractice.py

---
+++ addpractice.py
@@ -0,0 +1,20 @@
+num1=10
+num2=5
+def addition():
+  a=num1+num2
+  print(a)
+def sub():
+  s=num1-num2
+  print(s)
+def mul():
+  m= num1*num2
+  print(m)
+def div():
+  d=num1/num2
+  print(d)
+addition()
+sub()
+mul()
+div()
+
+  
